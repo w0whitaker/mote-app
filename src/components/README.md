df7e74389165d49e4cd30777d48dea2d3c23fc4f
William Whitaker <william@williamwhitaker.dev>
2023-04-17 13:13:29 -0400

    creates m-grid and m-grid-item components

---
+++ src/components/README.md
@@ -0,0 +1,8 @@
+# Components
+
+## Create new component
+
+1. npm create vite@latest
+2. npx storybook@latest init
+3. npx storybook@latest upgrade
+4. cd ./xxx npm install
--- src/components/m-box/.storybook/main.js
+++ src/components/m-box/.storybook/main.js
@@ -1,20 +1,14 @@
 /** @type { import('@storybook/web-components-vite').StorybookConfig } */
 import { vars, reset } from '../../../styles/styles';
-
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-a11y',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-a11y', '@storybook/addon-mdx-gfm'],
   framework: {
     name: '@storybook/web-components-vite',
-    options: {},
+    options: {}
   },
   docs: {
-    autodocs: 'tag',
+    autodocs: 'tag'
   },
   previewHead: head => `
   ${head}
@@ -24,7 +18,6 @@ const config = {
   <style>
     ${reset}
   </style>
-  `,
+  `
 };
-
 export default config;
